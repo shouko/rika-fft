8745c36fe4688ebe1e089cca917799a55bd27698
shouko <imshouko@gmail.com>
2017-11-07 17:01:58 +0800

    Update font style

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ http.createServer(function (req, res) {
   const text = decodeURI(req.url).substr(1)
   const size = 80
   const padding = 10
+  const font = size + 'px sans-serif'
 
   if (text.length == 0) {
     res.writeHead(400, {'Content-Type': 'text/plain'})
@@ -15,14 +16,15 @@ http.createServer(function (req, res) {
   
   const tmp_canvas = createCanvas()
   const tmp_ctx = tmp_canvas.getContext('2d')
-  tmp_ctx.font = size + 'px Impact'
+  tmp_ctx.font = font
   console.log('width', tmp_ctx.measureText(text).width, 'text', text)
   
   const canvas = createCanvas(tmp_ctx.measureText(text).width + padding * 2, size + padding * 2)
   const ctx = canvas.getContext('2d')
   
-  ctx.font = size + 'px Impact'
-  ctx.fillText(text, padding, (size + padding * 2) / 2)
+  ctx.font = font
+  ctx.textBaseline = 'hanging'
+  ctx.fillText(text, padding, 0)
 
   var stream = canvas.pngStream()
   stream.on('data', function(chunk){
